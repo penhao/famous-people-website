b1bdadea0ac99dd6a57a14fd65576ebfa8eea2da
penhao <penhao817@gmail.com>
2021-01-19 18:13:01 +0800

    [Update] SSL設定

--- dist/server.js
+++ dist/server.js
@@ -5,50 +5,27 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var next_1 = __importDefault(require("next"));
-var createServer = require('https').createServer;
-var parse = require('url').parse;
+var http = require('http');
+var https = require('https');
 var readFileSync = require('fs').readFileSync;
 var dev = process.env.NODE_ENV !== 'production';
-var port = parseInt(process.env.PORT, 10) || 3000;
-var sslPort = 443;
+var ports = {
+    dev: 3000, http: 80, https: 443
+};
 var app = next_1.default({ dev: dev });
 var handle = app.getRequestHandler();
-app.prepare()
-    .then(function () {
-    if (dev) {
-        var server = express_1.default();
-        server.all("*", function (req, res) {
-            return handle(req, res);
-        });
-        server.listen(dev ? port : sslPort, function (err) {
-            if (err)
-                throw err;
-            console.log("> Ready on localhost:" + port + " - env " + process.env.NODE_ENV);
-        });
-    }
-    else {
+var server = express_1.default();
+app.prepare().then(function () {
+    server.all('*', function (req, res) {
+        return handle(req, res);
+    });
+    http.createServer(server).listen(dev ? ports.dev : ports.http);
+    if (!dev) {
         var httpsOptions = {
             key: readFileSync('/home/centos/myserver.key'),
             ca: readFileSync('/home/centos/3A0E47BFC8835AD9412EEEBFE31AAEC2.cer'),
             cert: readFileSync('/home/centos/server.csr')
         };
-        createServer(httpsOptions, function (req, res) {
-            var parsedUrl = parse(req.url, true);
-            handle(req, res, parsedUrl);
-        }).listen(sslPort, function (err) {
-            if (err)
-                throw err;
-            console.log("> Ready on https://localhost:" + port);
-        });
+        https.createServer(httpsOptions, server).listen(ports.https);
     }
 });
-/*app.prepare().then(() => {
-    const server = express();
-    server.all("*", (req: Request, res: Response) => {
-        return handle(req, res);
-    });
-    server.listen(dev ? port : sslPort, (err?: any) => {
-        if (err) throw err;
-        console.log(`> Ready on localhost:${port} - env ${process.env.NODE_ENV}`);
-    });
-});*/
--- server/server.ts
+++ server/server.ts
@@ -1,50 +1,30 @@
 import express, {Request, Response} from "express";
 import next from "next";
 
-const {createServer} = require('https');
-const {parse} = require('url');
+const http = require('http');
+const https = require('https');
 const {readFileSync} = require('fs');
 
 const dev = process.env.NODE_ENV !== 'production';
-const port = parseInt(process.env.PORT, 10) || 3000;
-const sslPort = 443;
+const ports = {
+    dev: 3000, http: 80, https: 443
+};
 const app = next({dev});
 const handle = app.getRequestHandler();
+const server = express();
 
-app.prepare()
-    .then(() => {
-        if (dev) {
-            const server = express();
-            server.all("*", (req: Request, res: Response) => {
-                return handle(req, res);
-            });
-            server.listen(dev ? port : sslPort, (err?: any) => {
-                if (err) throw err;
-                console.log(`> Ready on localhost:${port} - env ${process.env.NODE_ENV}`);
-            });
-        } else {
-            const httpsOptions = {
-                key: readFileSync('/home/centos/myserver.key'),
-                ca: readFileSync('/home/centos/3A0E47BFC8835AD9412EEEBFE31AAEC2.cer'),
-                cert: readFileSync('/home/centos/server.csr')
-            };
-            createServer(httpsOptions, (req: Request, res: Response) => {
-                const parsedUrl = parse(req.url, true);
-                handle(req, res, parsedUrl);
-            }).listen(sslPort, (err?: any) => {
-                if (err) throw err;
-                console.log(`> Ready on https://localhost:${port}`);
-            })
-        }
-    });
 
-/*app.prepare().then(() => {
-    const server = express();
-    server.all("*", (req: Request, res: Response) => {
-        return handle(req, res);
+app.prepare().then(() => {
+    server.all('*', (req: Request, res: Response) => {
+        return handle(req, res)
     });
-    server.listen(dev ? port : sslPort, (err?: any) => {
-        if (err) throw err;
-        console.log(`> Ready on localhost:${port} - env ${process.env.NODE_ENV}`);
-    });
-});*/
+    http.createServer(server).listen(dev ? ports.dev : ports.http);
+    if (!dev) {
+        const httpsOptions = {
+            key: readFileSync('/home/centos/myserver.key'),
+            ca: readFileSync('/home/centos/3A0E47BFC8835AD9412EEEBFE31AAEC2.cer'),
+            cert: readFileSync('/home/centos/server.csr')
+        };
+        https.createServer(httpsOptions, server).listen(ports.https);
+    }
+});
